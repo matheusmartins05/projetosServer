1eabf3dd5a344df17b5ce89e632939df8cc3943d
matheusmartins05 <matheusmartins2901@gmail.com>
2024-07-18 09:04:48 -0300

    permisão para front acessar servidor

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import "dotenv/config";
 import app from "./src/app.js";
 
+CORS_ALLOWED_ORIGINS = [
+  "http://localhost:3000",
+]
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
