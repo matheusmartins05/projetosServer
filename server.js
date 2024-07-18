c62398256cdff9422edb4605b60d8d8a0e16f3c0
matheusmartins05 <matheusmartins2901@gmail.com>
2024-07-18 09:15:21 -0300

    acess control allow origin

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 import "dotenv/config";
 import app from "./src/app.js";
 
-CORS_ALLOWED_ORIGINS = [
-  "http://localhost:3000",
-]
 const PORT = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.listen(PORT, () => {
   console.log("servidor escutando!");
